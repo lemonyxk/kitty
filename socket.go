67599bbd108550bb3fff3ea42c7d66a8e1689285
lemo <652878191@qq.com>
2019-07-18 08:39:05 +0800

    change socket name

--- socket.go
+++ socket.go
@@ -55,7 +55,6 @@ type Connection struct {
 	Socket   *Socket
 	Response http.ResponseWriter
 	Request  *http.Request
-	mux      sync.RWMutex
 }
 
 // Socket conn
@@ -109,14 +108,14 @@ func (conn *Connection) EmitAll(fte *Fte, msg interface{}) {
 // Push 发送消息
 func (socket *Socket) Push(fd uint32, messageType int, msg []byte) error {
 
+	socket.mux.Lock()
+	defer socket.mux.Unlock()
+
 	conn, ok := socket.Connections[fd]
 
 	if !ok {
 		return fmt.Errorf("client %d is close", fd)
 	}
-
-	conn.mux.Lock()
-	defer conn.mux.Unlock()
 
 	// 默认为文本
 	if messageType == 0 {

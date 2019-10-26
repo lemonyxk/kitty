9d4b857e5f467f27519d237da8479935b50273e0
lemo <lemo@gamstudio.net>
2019-10-27 02:56:52 +0800

    init

--- utils/http_client_test.go
+++ utils/http_client_test.go
@@ -19,7 +19,7 @@ func TestPost(t *testing.T) {
 			name string
 			args args
 			want *httpClient
-		}{name: "hello", args: args{"http://baidu.com"}, want: NewHttpClient()},
+		}{name: "hello", args: args{"http://baidu.com"}, want: Post("http://baidu.com")},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {

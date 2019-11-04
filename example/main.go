a1672c0c5a74c5c16b29349bbbbbb1c47615a915
lemo <lemo@gamstudio.net>
2019-11-04 17:03:32 +0800

    1.add logger.

--- example/main.go
+++ example/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/Lemo-yxk/lemo/container"
+	"github.com/Lemo-yxk/lemo/utils"
 )
 
 func main() {
@@ -18,4 +20,9 @@ func main() {
 
 	q.Put(1)
 	log.Println(q.Get())
+
+	log.Println(utils.OrderID())
+	time.Sleep(time.Millisecond * 10)
+	log.Println(utils.OrderID())
+
 }
---
+++ utils/rand.go
@@ -0,0 +1,43 @@
+/**
+* @program: lemo
+*
+* @description:
+*
+* @author: lemo
+*
+* @create: 2019-11-04 16:15
+**/
+
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"math/big"
+	"strconv"
+	"time"
+)
+
+// [begin,end]
+func RandomInt(start int, end int) int {
+	if start > end {
+		panic("start can not bigger than end")
+	}
+	var number = new(big.Int).SetInt64(int64(end - start))
+	var randomNumber, _ = rand.Int(rand.Reader, number)
+	return int(randomNumber.Int64())
+}
+
+func UUID() string {
+	var bytes = make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, bytes); err != nil {
+		return ""
+	}
+	return hex.EncodeToString(bytes)
+}
+
+func OrderID() string {
+	var t = time.Now()
+	return strconv.FormatInt(t.UnixNano(), 10) + strconv.Itoa(RandomInt(10000, 99999))
+}

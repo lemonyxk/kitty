bb44a47a7543b710f9bcbc97af2e87c5615868b1
lemo <lemo@gamstudio.net>
2019-12-10 22:07:39 +0800

    1.spit the router.

--- go.mod
+++ go.mod
@@ -10,4 +10,5 @@ require (
 	github.com/jedib0t/go-pretty v4.3.0+incompatible
 	github.com/json-iterator/go v1.1.8
 	github.com/mattn/go-runewidth v0.0.7 // indirect
+	github.com/mitchellh/mapstructure v1.1.2
 )
--- utils/string.go
+++ utils/string.go
@@ -13,7 +13,6 @@ package utils
 import (
 	"bytes"
 	"fmt"
-	"strconv"
 )
 
 type str int
@@ -26,10 +25,6 @@ func (s str) Join(v []interface{}, sep string) string {
 
 	for i := 0; i < len(v); i++ {
 		switch v[i].(type) {
-		case int:
-			buf.WriteString(strconv.Itoa(v[i].(int)))
-		case float64:
-			buf.WriteString(strconv.FormatFloat(v[i].(float64), 'f', -1, 64))
 		case string:
 			buf.WriteString(v[i].(string))
 		default:

be3e2066fc37d5c313998b6e5154a34b2eb0b1b2
lemo <lemo@gamstudio.net>
2019-12-25 15:51:20 +0800

    1.change stack.

--- utils/string.go
+++ utils/string.go
@@ -19,10 +19,8 @@ type str int
 
 const String str = iota
 
-func (s str) Join(v []interface{}, sep string) string {
-
+func (s str) JoinInterface(v []interface{}, sep string) string {
 	var buf bytes.Buffer
-
 	for i := 0; i < len(v); i++ {
 		switch v[i].(type) {
 		case string:
@@ -34,6 +32,49 @@ func (s str) Join(v []interface{}, sep string) string {
 			buf.WriteString(sep)
 		}
 	}
-
 	return buf.String()
+}
+
+func (s str) Join(v []string, sep string) string {
+	var buf bytes.Buffer
+	for i := 0; i < len(v); i++ {
+		buf.WriteString(v[i])
+		if i != len(v)-1 {
+			buf.WriteString(sep)
+		}
+	}
+	return buf.String()
+}
+
+func (s str) ToIntInterface(v []interface{}) []int {
+	var res []int
+	for i := 0; i < len(v); i++ {
+		switch v[i].(type) {
+		case string:
+			res = append(res, Conv.Atoi(v[i].(string)))
+		case int:
+			res = append(res, v[i].(int))
+		case float64:
+			res = append(res, int(v[i].(float64)))
+		default:
+			res = append(res, Conv.Atoi(fmt.Sprintf("%v", v[i])))
+		}
+	}
+	return res
+}
+
+func (s str) ToInt(v []string) []int {
+	var res []int
+	for i := 0; i < len(v); i++ {
+		res = append(res, Conv.Atoi(v[i]))
+	}
+	return res
+}
+
+func (s str) ToFloat64(v []string) []float64 {
+	var res []float64
+	for i := 0; i < len(v); i++ {
+		res = append(res, Conv.StringToFloat64(v[i]))
+	}
+	return res
 }

5c655472cf277a9fbdb990ad3fbb545df19e3fb8
lemo <lemo@gamstudio.net>
2019-12-26 19:31:55 +0800

    1.change stack.

---
+++ utils/system.go
@@ -0,0 +1,36 @@
+/**
+* @program: lemo
+*
+* @description:
+*
+* @author: lemo
+*
+* @create: 2019-12-26 19:29
+**/
+
+package utils
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+type system int
+
+const System system = iota
+
+func (system system) OpenBrowser(url string) error {
+	var err error
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	return err
+}

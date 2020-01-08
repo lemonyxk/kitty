403a1a45fe799595c191211849458cb750ed56f2
lemo <lemo@gamstudio.net>
2020-01-08 11:57:17 +0800

    1.change stack.

---
+++ utils/signal_windows.go
@@ -0,0 +1,53 @@
+/**
+* @program: lemo
+*
+* @description:
+*
+* @author: lemo
+*
+* @create: 2019-10-22 17:44
+**/
+
+package utils
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type sig int
+
+const Signal sig = iota
+
+type done struct {
+	Done func(func(sig os.Signal))
+}
+
+func (s sig) ListenKill() *done {
+	var signalList = []os.Signal{syscall.SIGINT, syscall.SIGTERM}
+	// 创建信号
+	signalChan := make(chan os.Signal, 1)
+	// 通知
+	signal.Notify(signalChan, signalList...)
+	return &done{Done: func(f func(signal os.Signal)) {
+		// 阻塞
+		f(<-signalChan)
+		// 停止
+		signal.Stop(signalChan)
+	}}
+}
+
+func (s sig) Listen(sig ...os.Signal) *done {
+	var signalList = sig
+	// 创建信号
+	signalChan := make(chan os.Signal, 1)
+	// 通知
+	signal.Notify(signalChan, signalList...)
+	return &done{Done: func(f func(signal os.Signal)) {
+		// 阻塞
+		f(<-signalChan)
+		// 停止
+		signal.Stop(signalChan)
+	}}
+}
